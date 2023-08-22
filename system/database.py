034b1177070204e5fd65642532bf543db8de68c6
Mat1337 <matkovukovic25@gmail.com>
2023-08-23 01:52:16 +0200

    started working on the control panel

--- system/database.py
+++ system/database.py
@@ -1,2 +1,78 @@
 import sqlite3
 
+# connect to the database
+connection = sqlite3.connect("garage_system.db", check_same_thread=False)
+
+# get the cursor that is used for
+# communication with the database
+cursor = connection.cursor()
+
+# if the cars table does not exist
+# make sure that it is created
+cursor.execute(
+    """
+        CREATE TABLE IF NOT EXISTS
+        cars
+        (
+            id              INTEGER PRIMARY KEY,
+            first_name      VARCHAR(64),
+            last_name       VARCHAR(128),
+            licence_plate   VARCHAR(128)
+        )
+    """
+)
+
+# commit all the changes to the database
+connection.commit()
+
+
+def get_users():
+    return cursor.execute(
+        """
+            SELECT * FROM cars
+        """
+    ).fetchall()
+
+
+def check_user(first_name, last_name, licence_plate):
+    # execute the query that adds the user
+    # to the database
+    return len(cursor.execute(
+        """
+            SELECT * FROM cars WHERE first_name=? AND last_name=? AND licence_plate=?
+        """, (first_name, last_name, licence_plate)
+    ).fetchall()) > 0
+
+
+def create_user(first_name, last_name, licence_plate):
+    # if the user exists
+    # return out of the method
+    if check_user(first_name, last_name, licence_plate):
+        return False
+
+    # execute the query that adds the user
+    # to the database
+    cursor.execute(
+        """
+            INSERT INTO 
+                cars(id, first_name, last_name, licence_plate)
+            VALUES
+                (NULL, ?, ?, ?) 
+        """, (first_name, last_name, licence_plate)
+    )
+
+    # after the query has executed commit
+    # the changes to the database
+    connection.commit()
+
+    # return true meaning that
+    # user has been added
+    return True
+
+
+def check_licence_plate(licence_plate):
+    return cursor.execute(
+        """
+            SELECT first_name, last_name FROM cars WHERE licence_plate=?
+        """, [licence_plate]
+    ).fetchall()
--- system/server.py
+++ system/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template
-import os
+from flask import Flask, request, render_template, jsonify
+import database
 import detect
 import uuid
+import os
 
 # create the flask app
 app = Flask(__name__)
@@ -13,38 +14,88 @@ if not os.path.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
 
 
+def check_licence_plate(request):
+    if "image" in request.files:
+        # get the file that was uploaded
+        image = request.files["image"]
+
+        # make sure that the file was provided
+        if image.filename == "":
+            return None
+
+        # get the file extension (suffix) of the image
+        file_extension = os.path.splitext(image.filename)[1]
+
+        # generate a random uuid for the image
+        file_name = os.path.join(UPLOAD_DIR, str(uuid.uuid4())) + file_extension
+
+        # save the image
+        image.save(file_name)
+
+        # read the text from the licence plate
+        plate_text = detect.read_licence_plate_text(file_name)
+
+        # make sure that text was found on the licence plate
+        if plate_text is None or len(plate_text) == 0:
+            return None
+
+        # check if the licence plate has access to the garage
+        query_result = database.check_licence_plate(plate_text)
+
+        # make sure that text was found on the licence plate
+        if query_result is None or len(query_result) == 0:
+            return None
+
+        # get the first and last name of the user
+        first_name, last_name = query_result[0]
+
+        # return the variables
+        return first_name, last_name, plate_text
+
+    # if the image was not found make sure
+    # to return None
+    return None
+
+
+@app.route("/check", methods=["POST"])
+def check():
+    # check the licence plate
+    result = check_licence_plate(request)
+    if result is None:
+        return jsonify(error="You don't have access to this garage")
+
+    # get the first name, last name and the licence plate text from the request
+    first_name, last_name, plate_text = result
+
+    # return the data in the json format
+    return jsonify(
+        first_name=first_name,
+        last_name=last_name,
+        plate_text=plate_text
+    )
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        if "image" in request.files:
-            # get the file that was uploaded
-            image = request.files["image"]
+        # check the licence plate
+        result = check_licence_plate(request)
+        if result is None:
+            return render_template("responses/failure.html")
 
-            # make sure that the file was provided
-            if image.filename == "":
-                return render_template("failure.html")
+        # get the first name, last name and the licence plate text from the request
+        first_name, last_name, plate_text = result
 
-            # get the file extension (suffix) of the image
-            file_extension = os.path.splitext(image.filename)[1]
-
-            # generate a random uuid for the image
-            file_name = os.path.join(UPLOAD_DIR, str(uuid.uuid4())) + file_extension
-
-            # save the image
-            image.save(file_name)
-
-            # read the text from the licence plate
-            plate_text = detect.read_licence_plate_text(file_name)
-
-            # make sure that text was found on the licence plate
-            if plate_text is None or len(plate_text) == 0:
-                return render_template("responses/failure.html", licence_plate=plate_text)
-
-            # read the licence plate from the image and return it
-            return render_template("responses/success.html", licence_plate=plate_text)
+        # render the success page
+        return render_template("responses/success.html", response=f"{first_name} {last_name} - {plate_text}")
 
     # if the request was not post, return the default index.html file
     return render_template("index.html")
+
+
+@app.route("/admin", methods=["GET"])
+def admin():
+    return render_template("admin.html", results=database.get_users())
 
 
 # on the main entry point
